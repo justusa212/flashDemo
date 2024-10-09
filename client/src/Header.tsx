66a68598787735d8f0e079413e63072f32a0019e
Justin Wong <ohmygod@Justins-Laptop.local>
2024-10-09 17:45:26 -0400

    commit

---
+++ client/src/Header.tsx
@@ -0,0 +1,21 @@
+import "./Header.css";
+
+export function Header() {
+  return (
+    <div className="Header">
+      <div className="container">
+        <div>
+          <a href="/">FlashDemo</a>
+        </div>
+
+        <div>
+          <a href="/">Decks</a>
+        </div>
+
+        <div>
+          <a href="/">login</a>
+        </div>
+      </div>
+    </div>
+  );
+}
